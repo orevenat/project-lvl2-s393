5419599c1b92a417e984b58d5cbcb4e16245d954
Ruslan <agrry@yandex.ru>
2019-01-02 21:33:36 +0500

    added reading files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
-// import half from '..';
-
 import program from 'commander';
 import { version } from '../../package.json';
+import fs from 'fs';
+import genDiff from '..';
 
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
+  .action((before, after) => {
+    const beforeData = JSON.parse(fs.readFileSync(before, 'utf8'));
+    const afterData = JSON.parse(fs.readFileSync(after, 'utf8'));
+
+    return genDiff(beforeData, afterData);
+  })
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
