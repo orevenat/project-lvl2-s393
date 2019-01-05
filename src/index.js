0532ec4537021d7fae12d9d8e2013070045735aa
Ruslan <agrry@yandex.ru>
2019-01-05 09:07:34 +0500

    fix names

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import path from 'path';
 import _ from 'lodash';
 import parsers from './parsers';
 
-const readingFile = (filepath) => {
+const readFile = (filepath) => {
   const content = fs.readFileSync(filepath, 'utf-8');
   const fileExtension = path.extname(filepath);
   return [content, fileExtension];
 };
 
 export default (filepathBefore, filepathAfter) => {
-  const configBefore = parsers(...readingFile(filepathBefore));
-  const configAfter = parsers(...readingFile(filepathAfter));
-  const configKeysBefore = Object.keys(configBefore);
-  const configKeysAfter = Object.keys(configAfter);
-  const configKeysMerged = _.uniq(configKeysBefore.concat(configKeysAfter));
+  const configBefore = parsers(...readFile(filepathBefore));
+  const configAfter = parsers(...readFile(filepathAfter));
+  const configKeysMerged = _.union(_.keys(configBefore), _.keys(configAfter));
   const diffResult = configKeysMerged.map(key => ({
     name: key,
     oldValue: _.has(configBefore, key) ? configBefore[key] : null,
