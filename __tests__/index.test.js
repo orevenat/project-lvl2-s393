d21b42ea5e464d703e071f9e7a1f79109ee49bd8
Ruslan <agrry@yandex.ru>
2019-01-05 09:11:38 +0500

    rename fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,8 +6,8 @@ const fileTypes = ['.json', '.yml', '.ini'];
 
 const testList = (fileType) => {
   const fixturePath = path.resolve('__tests__/__fixtures__/');
-  const configOne = `${fixturePath}/config1${fileType}`;
-  const configTwo = `${fixturePath}/config2${fileType}`;
+  const configOne = `${fixturePath}/config_before${fileType}`;
+  const configTwo = `${fixturePath}/config_after${fileType}`;
   const expectedDiff = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf8');
 
   return [fileType, configOne, configTwo, expectedDiff];
